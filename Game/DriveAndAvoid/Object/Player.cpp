c5b27a52303fbcd9fa0b5325e457f4725d9ce79c
https-aya <138739258+https-aya@users.noreply.github.com>
2024-01-24 14:33:08 +0900

    player
    playerrcpp

--- Game/DriveAndAvoid/Object/Player.cpp
+++ Game/DriveAndAvoid/Object/Player.cpp
@@ -1,5 +1,5 @@
 #include "Player.h"
-#include "../Utility/Vector2D.h"
+#include "../Utility/InputControl.h"
 #include "DxLib.h"
 
 Player::Player() :is_active(false), image(NULL), location(0.0f), box_size(0.0f),
@@ -33,4 +33,99 @@ void Player::Initialize()
 	{
 		throw("Resource/images/car1po1.bmpがありません\n");
 	}
+}
+
+//更新処理
+void Player::Update()
+{
+	//操作不可状態であれば、自信を回転させる
+	if (!is_active)
+	{
+		angle += DX_PI_F / 24.0f;
+		speed = 1.0f;
+		if (angle >= DX_PI_F * 4.0f)
+		{
+			is_active = true;
+		}
+		return;
+	}
+
+	//燃料の消費
+	fuel -= speed;
+
+	//移動処理
+	Movement();
+
+	//加減速処理
+	Acceleration();
+
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_START))
+	{
+		is_active = false;
+	}
+
+	//バリア処理
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B) && barrier_count > 0)
+	{
+		if (barrier == nullptr)
+		{
+			barrier_count--;
+			barrier = new Barrier;
+		}
+	}
+
+	//バリアが生成されていたら、更新を行う
+	if (barrier != nullptr)
+	{
+		//バリア時間が経過したか？していたら削除する
+		if (barrier->IsFinished(this->speed))
+		{
+			delete barrier;
+			barrier = nullptr;
+		}
+	}
+}
+
+//描画処理
+void Player::Draw()
+{
+	//プレイヤー画像の描画
+	DrawRotaGraphF(location.x, location.y, 1.0, angle, image, TRUE);
+
+	//バリアが生成されていたら、描画を行う
+	if (barrier != nullptr)
+	{
+		barrier->Draw(this->location);
+	}
+}
+
+//終了時処理
+void Player::Finalize()
+{
+	//読み込んだ画像を削除
+	DeleteGraph(image);
+
+	//バリアが生成されていたら、削除する
+	if (barrier != nullptr)
+	{
+		delete barrier;
+	}
+}
+
+//状態設定処理
+void Player::SetActive(bool flg)
+{
+	this->is_active = flg;
+}
+
+//体力減少処理
+void Player::DecreaseHp(float value)
+{
+	this->hp += value;
+}
+
+//位置情報取得処理
+Vector2D Player::GetLocation() const
+{
+	return this->location;
 }
